518146f54e1b4478e6166ac895bf676b9764f400
Artem Zirka <zirkaartem@gmail.com>
2025-09-23 13:05:05 +0300

    Add JobTrackerServiceProvider and register it in composer.json

---
+++ src/JobTrackerServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\ServiceProvider;
+
+class JobTrackerServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+    }
+}
