98972f5e2e7e1ec16422241f331ad872b19b0b13
Artem Zirka <zirkaartem@gmail.com>
2025-09-24 12:48:11 +0300

    Add JobGroup and JobRecord models with relationships and table configuration

---
+++ src/Models/JobGroup.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AZirka\JobTracker\Models;
+
+use Enum\JTGroupStatus;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class JobGroup extends Model
+{
+    protected $fillable = [
+        'title',
+        'status',
+        'time_to_check',
+        'next_check_at',
+        'description',
+    ];
+
+    protected $casts = [
+        'status'        => JTGroupStatus::class,
+        'next_check_at' => 'datetime',
+        'payload'       => 'array',
+    ];
+
+    protected static function booted(): void
+    {
+        static::creating(function (JobGroup $model) {
+            if (is_null($model->next_check_at)) {
+                $ttc = $model->time_to_check ?? config('job-tracker.ttc');
+                $model->next_check_at = now()->addSeconds($ttc);
+            }
+        });
+
+        static::updating(function (JobGroup $model) {
+            if ($model->isDirty('time_to_check') && is_null($model->next_check_at)) {
+                $model->next_check_at = now()->addSeconds($model->time_to_check);
+            }
+        });
+    }
+
+    public function getTable(): string
+    {
+        return config('job-tracker.tables.groups');
+    }
+
+    public function jobRecords(): HasMany
+    {
+        return $this->hasMany(JobRecord::class, getForeignIdColumnName(config('job-tracker.tables.groups')), 'id');
+    }
+}
---
+++ src/Models/JobRecord.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AZirka\JobTracker\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class JobRecord extends Model
+{
+    protected $fillable = [
+        'uuid',
+    ];
+
+    public function getFillable(): array
+    {
+        return [...parent::getFillable(), [getForeignIdColumnName(config('job-tracker.tables.groups'))]];
+    }
+
+    public function getTable(): string
+    {
+        return config('job-tracker.tables.jobs');
+    }
+
+    public function jobGroup(): BelongsTo
+    {
+        return $this->belongsTo(JobGroup::class, getForeignIdColumnName(config('job-tracker.tables.groups')), 'id');
+    }
+}
