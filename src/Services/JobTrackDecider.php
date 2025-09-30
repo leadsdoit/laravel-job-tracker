9dc840bfab0bc41c4f39e0fd673111b8e86727a6
Artem Zirka <zirkaartem@gmail.com>
2025-09-30 14:43:33 +0300

    Added JobTrackDecider class for defining is job trackable

---
+++ src/Services/JobTrackDecider.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AZirka\JobTracker\Services;
+
+use AZirka\JobTracker\Contracts\JTTrackableJob;
+use Illuminate\Contracts\Queue\Job;
+
+final class JobTrackDecider
+{
+    public function isJobTrackable(Job $job): bool
+    {
+        $class = $this->resolveJobClass($job);
+        if ($class !== null) {
+            return is_subclass_of($class, JTTrackableJob::class);
+        }
+
+        return false;
+    }
+
+    public function resolveJobClass(Job $job): ?string
+    {
+        if (method_exists($job, 'resolveName')) {
+            $name = $job->resolveName();
+            if ($name === 'Illuminate\Queue\CallQueuedClosure') {
+                return null;
+            }
+            return class_exists($name) ? $name : null;
+        }
+
+        $payload = $job->payload();
+        if (!empty($payload['data']['commandName']) && class_exists($payload['data']['commandName'])) {
+            return $payload['data']['commandName'];
+        }
+
+        if (!empty($payload['displayName']) && class_exists($payload['displayName'])) {
+            return $payload['displayName'];
+        }
+
+        return null;
+    }
+}
